09d91b44ea450f774321b83406840faf4a277bad
JoseRT23 <joser16torres@gmail.com>
2025-12-26 20:33:16 -0500

    Changed error message in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,7 +4,7 @@ import { jwtAdapter } from "../config/jwt.adapter";
 const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
 
-    if (!token) return res.status(401).json({ message: 'No token' });
+    if (!token) return res.status(401).json({ message: 'No token provided' });
     const decoded = await jwtAdapter.validateToken(token);
     if (!decoded) return res.status(401).json({ message: 'Invalid token' });
     req.user = decoded;
